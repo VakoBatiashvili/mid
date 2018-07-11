8459a8534abc6bd479dd5bbadcf3f23948661e42
VakoBatiashvili <39788868+VakoBatiashvili@users.noreply.github.com>
2018-07-11 11:34:43 +0400

    Add files via upload

---
+++ caser.py
@@ -0,0 +1,12 @@
+from string import ascii_lowercase as al
+dashifruli=list(input("sheiyvanet dashifruli sityva"))
+for i in range(0,25):
+    x=""
+    for m in dashifruli:
+        ind=al.index(m)+i
+        if ind>25:
+            ind-=25
+        
+        x+=al[ind]
+    print(x,i+1)
+            
