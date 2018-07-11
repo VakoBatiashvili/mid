42e544e2d2c08ebdd148d5f2029596624c4599ce
VakoBatiashvili <39788868+VakoBatiashvili@users.noreply.github.com>
2018-07-11 12:36:47 +0400

    Update caser.py

--- caser.py
+++ caser.py
@@ -1,6 +1,6 @@
 from string import ascii_lowercase as al
 dashifruli=list(input("sheiyvanet dashifruli sityva"))
-for i in range(0,25):
+for i in range(0,26):
     x=""
     for m in dashifruli:
         ind=al.index(m)+i
